9042b15ed9601e41c709631d29b608baeb55c0cf
shurangit <shurangithub@gmail.com>
2015-09-21 09:35:18 -0700

    python c++

--- Unique Paths .cpp
+++ Unique Paths .cpp
@@ -17,3 +17,18 @@ public:
         return sum[m-1][n-1];
     }
 };
+
+//python
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp=[[1]*n]*m
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
+        
